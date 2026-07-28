090eecb2a811a081994b36619a9ed7fc23e6d8ce
tincandream <diannavmwaka@gmail.com>
2026-07-27 19:32:23 -0500

    Update regions.js

--- js/regions.js
+++ js/regions.js
@@ -1,1 +1,30 @@
+const REGIONS = {
+    southeast: [
+        "KY", "WV", "VA", "NC", "SC", "GA",
+        "FL", "TN", "AL", "MS", "LA", "AR"
+    ],
 
+    southwest: [
+        "TX", "OK", "NM", "AZ"
+    ],
+
+    midwest: [
+        "ND", "SD", "NE", "KS",
+        "MN", "IA", "MO",
+        "WI", "IL", "IN", "MI", "OH"
+    ],
+
+    northeast: [
+        "ME", "NH", "VT",
+        "MA", "RI", "CT",
+        "NY", "NJ", "PA",
+        "MD", "DE"
+    ],
+
+    west: [
+        "WA", "OR", "CA",
+        "NV", "ID", "MT",
+        "WY", "UT", "CO",
+        "AK", "HI"
+    ]
+};
